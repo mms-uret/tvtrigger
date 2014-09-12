9e574d338e2cfb1b3f18ba2236ff85bca104103c
Urban Etter <urban.etter@srf.ch>
2014-09-12 15:57:41 +0200

    Send channel as well

--- web/index.php
+++ web/index.php
@@ -11,10 +11,13 @@ $app->get('/', function() {
 
     $xml = $response->xml();
     $times = array();
+    $channels = array();
     foreach ($xml->channel->item as $item) {
         $title = (string)$item->title;
         $date = explode(',', $title)[0];
         $times[] = strtotime($date);
+        $channel = explode(':', explode(',', $title)[1])[0];
+        $channels[strtotime($date)] = $channel;
     }
 
     $next = min($times);
@@ -28,7 +31,8 @@ $app->get('/', function() {
     $trigger = false;
     if ($now > $next - $tolerance && $now < $next) {
         $trigger = true;
-        $client->put('http://requestb.in/p7ne8hp7')->send();
+        $client->put('http://tamberg-homer.try.yaler.net/ir?key=1&channel=' . $channel)->send();
+        //$client->put('http://requestb.in/p7ne8hp7?key=1&channel=' . $channel)->send();
     }
 
     $result = "Now: " . date('r', $now) . "<br>Next show: " . date('r', $next) . "<br>Trigger: " . (($trigger) ? "true" : "false");
