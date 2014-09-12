2cbbe1a3b1f78364bb03612f9b44b3dbbaa4d916
Urban Etter <urban.etter@srf.ch>
2014-09-12 15:30:57 +0200

    Basic implementation

--- web/index.php
+++ web/index.php
@@ -3,12 +3,37 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->get('/', function() {
     $client = new Guzzle\Http\Client();
-    $response = $client->get('http://tvprogramm.srf.ch/feed/q/query/Tagesschau/startd/2014-09-12/endd/2014-09-12/starth/00/endh/24');
-    //echo $response->getStatusCode();
-    return $response->getBody(true);
+    $response = $client->get('http://tvprogramm.srf.ch/feed/q/query/simpsons')->send();
+
+    $xml = $response->xml();
+    $times = array();
+    foreach ($xml->channel->item as $item) {
+        $title = (string)$item->title;
+        $date = explode(',', $title)[0];
+        $times[] = strtotime($date);
+    }
+
+    $next = min($times);
+
+    $tolerance = 5 * 60;
+
+    $now = time();
+    if (isset($_GET['now'])) {
+        $now = strtotime($_GET['now']);
+    }
+    $trigger = false;
+    if ($now > $next - $tolerance && $now < $next) {
+        $trigger = true;
+        $client->put('http://requestb.in/p7ne8hp7')->send();
+    }
+
+    $result = "Now: " . date('r', $now) . "<br>Next show: " . date('r', $next) . "<br>Trigger: " . (($trigger) ? "true" : "false");
+
+    return $result;
 });
 
 $app->run();
