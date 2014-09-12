dc0c316b3368483a1f11a0d60f2befba4c8e2bd2
Urban Etter <urban.etter@srf.ch>
2014-09-12 14:05:32 +0200

    Guzzle stuff

--- web/index.php
+++ web/index.php
@@ -5,7 +5,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function() {
-    return "Hoi Gianni";
+    $client = new Guzzle\Http\Client();
+    $response = $client->get('http://tvprogramm.srf.ch/feed/q/query/Tagesschau/startd/2014-09-12/endd/2014-09-12/starth/00/endh/24');
+    echo $response->getStatusCode();
+    echo $response->getBody(true);
 });
 
 $app->run();
